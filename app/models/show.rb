02f36319c09ace8ddd3179905e4af56ddc630348
Marie Burns <mcburns2222@gmail.com>
2019-02-16 21:39:21 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :characters 
   has_many :actors, through: :characters
   
-  def build_network
+  def build_network(arg = {})
     binding.pry
   end
 end
