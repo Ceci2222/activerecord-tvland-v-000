4293c4d59e95d931657ae759612948a8ef458e1f
Marie Burns <mcburns2222@gmail.com>
2019-02-16 21:41:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ require 'pry'
 class Show < ActiveRecord::Base
   has_many :characters 
   has_many :actors, through: :characters
+  belongs_to :network
   
   def build_network(arg = {})
     binding.pry
